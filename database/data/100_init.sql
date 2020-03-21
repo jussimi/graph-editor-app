2b7c8d9e07a3475e4fa6840aae01a8d10409ba8c
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2020-03-22 00:39:41 +0200

    added .env file from which the docker containers are built

--- database/data/100_init.sql
+++ database/data/100_init.sql
@@ -1,21 +1,21 @@
 BEGIN;
 
-CREATE SCHEMA graph_editor;
-CREATE SCHEMA graph_editor_private;
+CREATE SCHEMA ${GRAPH_EDITOR_SCHEMA};
+CREATE SCHEMA ${GRAPH_EDITOR_SCHEMA}_private;
 
-CREATE ROLE graph_editor_postgraphile LOGIN PASSWORD 'graph_editor_postgraphile_pass';
-CREATE ROLE graph_editor_anonymous;
-CREATE ROLE graph_editor_person;
+CREATE ROLE ${GRAPH_EDITOR_SCHEMA}_postgraphile LOGIN PASSWORD '${GRAPH_EDITOR_USER_PASS}';
+CREATE ROLE ${GRAPH_EDITOR_SCHEMA}_anonymous;
+CREATE ROLE ${GRAPH_EDITOR_SCHEMA}_person;
 
-GRANT graph_editor_anonymous TO graph_editor_postgraphile;
-GRANT graph_editor_person TO graph_editor_postgraphile;
-GRANT graph_editor_postgraphile TO testuser;
+GRANT ${GRAPH_EDITOR_SCHEMA}_anonymous TO ${GRAPH_EDITOR_SCHEMA}_postgraphile;
+GRANT ${GRAPH_EDITOR_SCHEMA}_person TO ${GRAPH_EDITOR_SCHEMA}_postgraphile;
+GRANT ${GRAPH_EDITOR_SCHEMA}_postgraphile TO ${DB_USER};
 
-GRANT USAGE ON SCHEMA graph_editor TO graph_editor_postgraphile;
-GRANT USAGE ON SCHEMA graph_editor TO graph_editor_anonymous;
-GRANT USAGE ON SCHEMA graph_editor TO graph_editor_person;
+GRANT USAGE ON SCHEMA ${GRAPH_EDITOR_SCHEMA} TO ${GRAPH_EDITOR_SCHEMA}_postgraphile;
+GRANT USAGE ON SCHEMA ${GRAPH_EDITOR_SCHEMA} TO ${GRAPH_EDITOR_SCHEMA}_anonymous;
+GRANT USAGE ON SCHEMA ${GRAPH_EDITOR_SCHEMA} TO ${GRAPH_EDITOR_SCHEMA}_person;
 
---GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA graph_editor TO graph_editor_postgraphile;
+--GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA ${GRAPH_EDITOR_SCHEMA} TO ${GRAPH_EDITOR_SCHEMA}_postgraphile;
 
 ALTER DEFAULT PRIVILEGES REVOKE EXECUTE ON FUNCTIONS FROM PUBLIC;
 
@@ -28,147 +28,147 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE TABLE graph_editor.person (
+CREATE TABLE ${GRAPH_EDITOR_SCHEMA}.person (
   id                   serial PRIMARY KEY,
   email                text NOT NULL UNIQUE CHECK(email ~* '^.+@.+\..+$'),
   created_at           timestamp DEFAULT NOW() NOT NULL,
   updated_at           timestamp DEFAULT NOW() NOT NULL
 );
 
-ALTER TABLE graph_editor.person ENABLE ROW LEVEL SECURITY;
-ALTER SEQUENCE graph_editor.person_id_seq RESTART WITH 111;
+ALTER TABLE ${GRAPH_EDITOR_SCHEMA}.person ENABLE ROW LEVEL SECURITY;
+ALTER SEQUENCE ${GRAPH_EDITOR_SCHEMA}.person_id_seq RESTART WITH 111;
 
-CREATE TRIGGER set_timestamp BEFORE UPDATE ON graph_editor.person
+CREATE TRIGGER set_timestamp BEFORE UPDATE ON ${GRAPH_EDITOR_SCHEMA}.person
 FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
 
-GRANT USAGE, SELECT ON SEQUENCE graph_editor.person_id_seq TO graph_editor_person;
-GRANT SELECT, UPDATE(email) ON TABLE graph_editor.person TO graph_editor_person;
+GRANT USAGE, SELECT ON SEQUENCE ${GRAPH_EDITOR_SCHEMA}.person_id_seq TO ${GRAPH_EDITOR_SCHEMA}_person;
+GRANT SELECT, UPDATE(email) ON TABLE ${GRAPH_EDITOR_SCHEMA}.person TO ${GRAPH_EDITOR_SCHEMA}_person;
 
-CREATE POLICY graph_editor_person_all_access_to_person
-  ON graph_editor.person
+CREATE POLICY ${GRAPH_EDITOR_SCHEMA}_person_all_access_to_person
+  ON ${GRAPH_EDITOR_SCHEMA}.person
   FOR ALL
-  TO graph_editor_person
+  TO ${GRAPH_EDITOR_SCHEMA}_person
   USING (id = current_setting('jwt.claims.person_id')::int);
 
 
 
 
-CREATE TABLE graph_editor.graph (
+CREATE TABLE ${GRAPH_EDITOR_SCHEMA}.graph (
   id                   serial PRIMARY KEY,
-  person_id            integer REFERENCES graph_editor.person(id) ON DELETE CASCADE NOT NULL,
+  person_id            integer REFERENCES ${GRAPH_EDITOR_SCHEMA}.person(id) ON DELETE CASCADE NOT NULL,
   data                 JSONB DEFAULT '{}' NOT NULL,
   updated_at           timestamp DEFAULT NOW() NOT NULL,
   created_at           timestamp DEFAULT NOW() NOT NULL
 );
-ALTER TABLE graph_editor.graph ENABLE ROW LEVEL SECURITY;
-ALTER SEQUENCE graph_editor.graph_id_seq RESTART WITH 111;
+ALTER TABLE ${GRAPH_EDITOR_SCHEMA}.graph ENABLE ROW LEVEL SECURITY;
+ALTER SEQUENCE ${GRAPH_EDITOR_SCHEMA}.graph_id_seq RESTART WITH 111;
 
-CREATE TRIGGER set_timestamp BEFORE UPDATE ON graph_editor.graph
+CREATE TRIGGER set_timestamp BEFORE UPDATE ON ${GRAPH_EDITOR_SCHEMA}.graph
 FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
 
-GRANT USAGE, SELECT ON SEQUENCE graph_editor.graph_id_seq TO graph_editor_person;
-GRANT SELECT, INSERT(person_id, data), UPDATE(data), DELETE ON TABLE graph_editor.graph TO graph_editor_person;
+GRANT USAGE, SELECT ON SEQUENCE ${GRAPH_EDITOR_SCHEMA}.graph_id_seq TO ${GRAPH_EDITOR_SCHEMA}_person;
+GRANT SELECT, INSERT(person_id, data), UPDATE(data), DELETE ON TABLE ${GRAPH_EDITOR_SCHEMA}.graph TO ${GRAPH_EDITOR_SCHEMA}_person;
 
-CREATE POLICY graph_editor_person_all_access_to_graph
-  ON graph_editor.graph
+CREATE POLICY ${GRAPH_EDITOR_SCHEMA}_person_all_access_to_graph
+  ON ${GRAPH_EDITOR_SCHEMA}.graph
   FOR ALL
-  TO graph_editor_person
+  TO ${GRAPH_EDITOR_SCHEMA}_person
   USING (person_id = current_setting('jwt.claims.person_id')::int);
 
 
 
 
-CREATE TABLE graph_editor_private.person_account (
-  person_id            integer REFERENCES graph_editor.person(id) ON DELETE CASCADE NOT NULL,
+CREATE TABLE ${GRAPH_EDITOR_SCHEMA}_private.person_account (
+  person_id            integer REFERENCES ${GRAPH_EDITOR_SCHEMA}.person(id) ON DELETE CASCADE NOT NULL,
   password_hash        text NOT NULL,
   created_at           timestamp DEFAULT NOW() NOT NULL,
   updated_at           timestamp DEFAULT NOW() NOT NULL,
   UNIQUE(person_id)
 );
-ALTER TABLE graph_editor.person ENABLE ROW LEVEL SECURITY;
+ALTER TABLE ${GRAPH_EDITOR_SCHEMA}.person ENABLE ROW LEVEL SECURITY;
 
-CREATE TRIGGER set_timestamp BEFORE UPDATE ON graph_editor_private.person_account
+CREATE TRIGGER set_timestamp BEFORE UPDATE ON ${GRAPH_EDITOR_SCHEMA}_private.person_account
 FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
 
 
 
-CREATE TYPE graph_editor.auth_token as (
+CREATE TYPE ${GRAPH_EDITOR_SCHEMA}.auth_token as (
   role text,
   person_id integer,
   exp bigint
 );
 
-CREATE FUNCTION graph_editor.authenticate(
+CREATE FUNCTION ${GRAPH_EDITOR_SCHEMA}.authenticate(
   email text,
   password text
-) RETURNS graph_editor.auth_token AS $$
+) RETURNS ${GRAPH_EDITOR_SCHEMA}.auth_token AS $$
 DECLARE
-  account graph_editor_private.person_account;
+  account ${GRAPH_EDITOR_SCHEMA}_private.person_account;
 BEGIN
   SELECT a.* INTO account
-  FROM graph_editor_private.person_account a, graph_editor.person p
+  FROM ${GRAPH_EDITOR_SCHEMA}_private.person_account a, ${GRAPH_EDITOR_SCHEMA}.person p
   WHERE p.email = $1 AND a.person_id = p.id;
 
   IF account.password_hash = crypt($2, account.password_hash) THEN
     RETURN (
-      'graph_editor_person',
+      '${GRAPH_EDITOR_SCHEMA}_person',
       account.person_id,
       EXTRACT(epoch FROM (NOW() + interval '2 days'))
-    )::graph_editor.auth_token;
+    )::${GRAPH_EDITOR_SCHEMA}.auth_token;
   ELSE
     RETURN NULL;
   END IF;
 END;
 $$ language plpgsql strict security definer;
 
-GRANT EXECUTE ON FUNCTION graph_editor.authenticate(text, text) TO graph_editor_anonymous;
+GRANT EXECUTE ON FUNCTION ${GRAPH_EDITOR_SCHEMA}.authenticate(text, text) TO ${GRAPH_EDITOR_SCHEMA}_anonymous;
 
 
-CREATE FUNCTION graph_editor.register_person(
+CREATE FUNCTION ${GRAPH_EDITOR_SCHEMA}.register_person(
   email text,
   password text
-) RETURNS graph_editor.auth_token as $$
+) RETURNS ${GRAPH_EDITOR_SCHEMA}.auth_token as $$
 DECLARE
-  person graph_editor.person;
+  person ${GRAPH_EDITOR_SCHEMA}.person;
 BEGIN
-  INSERT INTO graph_editor.person(email)
+  INSERT INTO ${GRAPH_EDITOR_SCHEMA}.person(email)
   VALUES ($1)
   RETURNING * INTO person;
 
-  INSERT INTO graph_editor_private.person_account (person_id, password_hash)
+  INSERT INTO ${GRAPH_EDITOR_SCHEMA}_private.person_account (person_id, password_hash)
   VALUES (person.id, crypt(password, gen_salt('bf')));
 
   RETURN (
-    'graph_editor_person',
+    '${GRAPH_EDITOR_SCHEMA}_person',
     person.id,
     EXTRACT(epoch FROM (NOW() + interval '2 days'))
-  )::graph_editor.auth_token;
+  )::${GRAPH_EDITOR_SCHEMA}.auth_token;
 END;
 $$ language plpgsql strict security definer;
 
-GRANT EXECUTE ON FUNCTION graph_editor.register_person(text, text) TO graph_editor_anonymous;
+GRANT EXECUTE ON FUNCTION ${GRAPH_EDITOR_SCHEMA}.register_person(text, text) TO ${GRAPH_EDITOR_SCHEMA}_anonymous;
 
 
-CREATE FUNCTION graph_editor.remove_person(
+CREATE FUNCTION ${GRAPH_EDITOR_SCHEMA}.remove_person(
   email text
 ) RETURNS BOOLEAN AS $$
 DECLARE
   person_id_from_jwt integer := current_setting('jwt.claims.person_id')::int;
-  person graph_editor.person;
+  person ${GRAPH_EDITOR_SCHEMA}.person;
 BEGIN
-  SELECT p.* INTO person FROM graph_editor.person p WHERE p.email = $1;
+  SELECT p.* INTO person FROM ${GRAPH_EDITOR_SCHEMA}.person p WHERE p.email = $1;
 
   IF (person.id != person_id_from_jwt) THEN
     RAISE EXCEPTION 'unauthorized';
   END IF;
   
-  DELETE FROM graph_editor.person WHERE id = person.id;
+  DELETE FROM ${GRAPH_EDITOR_SCHEMA}.person WHERE id = person.id;
 
   RETURN true;
 END;
 $$ language plpgsql strict security definer;
 
-GRANT EXECUTE ON FUNCTION graph_editor.remove_person(text) TO graph_editor_person;
+GRANT EXECUTE ON FUNCTION ${GRAPH_EDITOR_SCHEMA}.remove_person(text) TO ${GRAPH_EDITOR_SCHEMA}_person;
 
 
 COMMIT;
