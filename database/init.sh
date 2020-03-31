c49d039e1f35cfbab568a7520274fae672be2999
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2020-03-31 15:32:41 +0300

    initial testing

--- database/init.sh
+++ database/init.sh
@@ -10,13 +10,3 @@ EOSQL
 psql testdb postgres <<-EOSQL
     CREATE EXTENSION pgcrypto;
 EOSQL
-
-# Loop through all files in data (note that we have been given write access in Dockerfile)
-FILES=/tmp_data/*
-for file in $FILES
-do
-  f=`basename $file`
-  envsubst < "$file" > "data/$f"
-  # migrate each file
-  psql testdb testuser -f "data/$f"
-done
