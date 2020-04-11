4d4dc9b75f765bbc61288a044af211e465a3b4b9
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2020-04-12 00:33:50 +0300

    created dockerfile

--- e2e/cypress/support/global.d.ts
+++ e2e/cypress/support/global.d.ts
@@ -2,6 +2,7 @@ export {};
 
 // NOTE: Nuxt saves the store to the window object when the client is Cypress.
 // This way we can use the actions from the app from cypress.
+// Look in /ui/plugins/cypress.ts for the hack.
 declare global {
   interface Window {
     store: {
@@ -10,4 +11,3 @@ declare global {
     };
   }
 }
-window.store = window.store || {};
