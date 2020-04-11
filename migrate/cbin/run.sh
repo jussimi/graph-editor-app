588dacafb9a61b225394923a6c67c0e134d5f011
jamaik <jamaik@utu.fi>
2020-04-12 00:34:16 +0300

    refactored directory structure

--- migrate/cbin/run.sh
+++ migrate/cbin/run.sh
@@ -3,23 +3,23 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $SCRIPT_DIR
 
-DATA_DIR=$SCRIPT_DIR/database
+SRC_DIR=$SCRIPT_DIR/src
 FIRST_COMMAND=$1
 MIGRATE_COMMANDS=${@}
 
-mkdir -p $DATA_DIR/tmp
+mkdir -p $SRC_DIR/tmp
 if [ "$FIRST_COMMAND" = "up" -o "$FIRST_COMMAND" = "down" -o "$FIRST_COMMAND" = "goto" ]; then
-  FILES=$DATA_DIR/migrations/*
+  FILES=$SRC_DIR/migrations/*
   for file in $FILES
   do
     f=`basename $file`
-    envsubst < "$file" > "$DATA_DIR/tmp/$f"
+    envsubst < "$file" > "$SRC_DIR/tmp/$f"
   done
 fi
 
-migrate -path /$DATA_DIR/tmp \
+migrate -path /$SRC_DIR/tmp \
   -verbose \
   -database $OWNER_DB_URL \
   $MIGRATE_COMMANDS
 
-rm -rf $DATA_DIR/tmp
+rm -rf $SRC_DIR/tmp
